7b126df0e6ff6a4dbcdf82732bf2a1d109a23e1e
Christian Shinkle <cshinkle2018@gmail.com>
2022-07-19 21:13:10 -0500

    refactored input parsing into function

--- main.py
+++ main.py
@@ -1,48 +1,41 @@
-from src.utils import sample_no_replace, is_empty_string
-
-all_engineer_set = {
-    'christian',
-    'girish',
-    'jason',
-    'kyle',
-    'manny',
-    'niko',
-    'tumsa',
-    'tyler',
-}
+from utils import parse_input_to_set, sample_no_replace
 
 
 def main():
-    raw_missing = input('Who is gone today? (Comma separated list)\n')
-    missing_set = set(token.strip().lower() for token in raw_missing.split(',') if not is_empty_string(token))
-    if not missing_set.issubset(all_engineer_set):
-        print('The engineers you listed are not a subset of the current team.')
-        print('Check for typos or update "all_engineers_set".')
-        print(missing_set)
+    all_engineer_set = {
+        'christian',
+        'girish',
+        'jason',
+        'kyle',
+        'manny',
+        'niko',
+        'tumsa',
+        'tyler',
+    }
+
+    missing_set: set[str] | None = parse_input_to_set('Who is gone today? (Comma separated list)\n',
+                                                      all_engineer_set)
+    if missing_set is None:
         return
 
     working_engineer_set = all_engineer_set - missing_set
 
-    raw_owners = input('Who owns a story? (Comma separated list)\n')
-    owner_set = set(token.strip().lower() for token in raw_owners.split(',') if not is_empty_string(token))
-    if not owner_set.issubset(all_engineer_set):
-        print('The engineers you listed are not a subset of the current team.')
-        print('Check for typos or update "all_engineers_set".')
-        print(owner_set)
+    owner_set = parse_input_to_set('Who owns a story? (Comma separated list)\n', all_engineer_set)
+    if owner_set is None:
         return
 
-    free_engineer_list = list(working_engineer_set - owner_set)
+    free_engineers = list(working_engineer_set - owner_set)
 
-    pairs = [(owner, sample_no_replace(free_engineer_list)) for owner in owner_set]
-    while len(free_engineer_list) >= 2:
-        pairs.append((sample_no_replace(free_engineer_list), sample_no_replace(free_engineer_list)))
+    pairs = [(owner, sample_no_replace(free_engineers)) for owner in owner_set]
+    while len(free_engineers) >= 2:
+        pairs.append((sample_no_replace(free_engineers), sample_no_replace(free_engineers)))
 
     print("Here's the pairs:")
     for pair in pairs:
         print(f'{pair[0]} + {pair[1]}')
 
-    if len(free_engineer_list) == 1:
-        print(f'{free_engineer_list[0]} is on his own!')
+    if len(free_engineers) == 1:
+        print(f'{free_engineers[0]} is on his own!')
 
 
 if __name__ == '__main__':
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,17 @@
 from random import choice
-from typing import List
+from typing import List, Optional, Set
+
+
+def parse_input_to_set(prompt: str, engineer_set: Set[str]) -> Optional[Set[str]]:
+    raw_input = input(prompt)
+    input_set = set(token.strip().lower() for token in raw_input.split(',') if not is_empty_string(token))
+    if input_set.issubset(engineer_set):
+        return input_set
+    else:
+        print('The engineers you listed are not a subset of the current team.')
+        print('Check for typos or update "all_engineers_set".')
+        print(input_set)
+        return None
 
 
 def is_empty_string(string: str) -> bool:
